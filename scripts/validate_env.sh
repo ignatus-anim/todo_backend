e5cb5bbf4c3fd0cb52bc02a50fcfafd663af6467
Ignatus <ignatusa3@gmail.com>
2024-12-07 21:52:37 +0000

    fix: update validate_env.sh

--- scripts/validate_env.sh
+++ scripts/validate_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check if required environment variables are set
-REQUIRED_VARS=("DB_HOST" "DB_PORT" "DB_NAME" "DB_USER" "DB_PASSWORD" "SECRET_KEY")
+REQUIRED_VARS=("DB_HOST" "DB_PORT" "DB_NAME" "DB_USER" "DB_PASSWORD")
 
 # Iterate over the required variables and check if they are set
 for var in "${REQUIRED_VARS[@]}"; do
